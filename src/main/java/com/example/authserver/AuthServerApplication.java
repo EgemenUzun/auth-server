ed6a25f19bf0bbf50ae32b60e7f531a22e019f57
Egemen <avniegemen1243@gmail.com>
2023-08-01 14:48:38 +0300

    jenkinsfile context change

--- src/main/java/com/example/authserver/AuthServerApplication.java
+++ src/main/java/com/example/authserver/AuthServerApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -17,8 +19,10 @@ import java.util.UUID;
 @SpringBootApplication
 public class AuthServerApplication {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintStream originalOut = System.out;
+        PrintStream fileOut = new PrintStream("./out.txt");
+        System.setOut(fileOut);
         SpringApplication.run(AuthServerApplication.class, args);
     }
 }
